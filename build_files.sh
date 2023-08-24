06a63ac1aa03043d305e43b019aac64a62e76ece
kewaltakhe <kewaltakhellambam5@gmail.com>
2023-08-24 14:34:27 +0530

    python3 in build file

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 # build_files.sh
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 
 # make migrations
-python3 manage.py migrate 
-python3 manage.py collectstatic
+python3.9 manage.py migrate 
+python3.9 manage.py collectstatic
